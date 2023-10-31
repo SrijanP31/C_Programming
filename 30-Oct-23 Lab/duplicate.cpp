0d233982049b143106e0471cf0a82dd885e2823c
SrijanP31 <143769604+SrijanP31@users.noreply.github.com>
2023-10-31 20:27:30 +0530

    Add files via upload

--- 30-Oct-23 Lab/duplicate.cpp
+++ 30-Oct-23 Lab/duplicate.cpp
@@ -1,23 +1,39 @@
 #include <stdio.h>
 
 int main(){
-	int n;
+	int n,d=0;
 	printf("Enter number of elements in array:");
 	scanf("%d", &n);
-	int arr[n];
+	int arr[n],arr_dup[n];
 	printf("Enter the values of elements:");
 	for(int i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	}
-	for(int i=0;i<n;i++){
-		int c=0;
+  	for(int i=0;i<n;i++){
+		int c = 0;
 		for(int j=0;j<n;j++){
-			if(arr[i]==arr[j]){
+			if(arr[i] == arr[j]){
 				c++;
 			}
 		}
-		if(c==1){	
-			printf("%d \n",arr[i]);
+		if(c>1){
+			int f=0;
+			for(int k=0;k<d;k++){
+				if(arr[i] == arr_dup[k]){
+					f++;
+					break;
+				}
+			}
+			if(f==0){
+				arr_dup[d] = arr[i];
+				d++;
+			}
+			
 		}
-}
+	}
+	
+	for(int i=0;i<d;i++){
+		printf("%d ",arr_dup[i]);
+	}
+	
 }
