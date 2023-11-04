d7ae7fb614632a7a5e44a46ee6dec91e0a0ccf3f
SrijanP31 <143769604+SrijanP31@users.noreply.github.com>
2023-11-04 19:48:14 +0530

    Update diag_sum.cpp

--- 16-Oct-23 Lab/diag_sum.cpp
+++ 16-Oct-23 Lab/diag_sum.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(){
-	int n=3,s=0,t=0;
+	int n,s=0,t=0;
+	printf("Enter the value of n:");
+	scanf("%d",&n);
 	int a[n][n];
 	printf("Enter values of matrix 1: \n");
 	for(int i=0;i<n;i++){
@@ -15,6 +17,6 @@ int main(){
 	for(int j=0;j<n;j++){
 		t += a[j][n-j-1];
 	}
-	printf("Sum of principle diagonal: %d\n", s);
-	printf("Sum of other diagonal: %d\n",t);
+	printf("Sum of left diagonal: %d\n", s);
+	printf("Sum of right diagonal: %d\n",t);
 }
