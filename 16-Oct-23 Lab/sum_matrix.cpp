431f0afc871939c776f28117d548a43f41c1ce44
SrijanP31 <143769604+SrijanP31@users.noreply.github.com>
2023-11-05 18:38:50 +0530

    Update sum_matrix.cpp

--- 16-Oct-23 Lab/sum_matrix.cpp
+++ 16-Oct-23 Lab/sum_matrix.cpp
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
 int main(){
-	int a[2][2], b[2][2], c[2][2];
+	int n;
+	printf("Enter a value for n:");
+	scanf("%d",&n);
+	int a[n][n], b[n][n], c[n][n];
 	printf("Enter values of matrix 1: \n");
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			scanf("%d",&a[i][j]);
 		}
 	}
 	printf("Enter values of matrix 2:\n");
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			scanf("%d",&b[i][j]);
 		}
 	}
 	
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			c[i][j] = a[i][j] + b[i][j];
 		}
 	}
 	printf("Sum of matrices 1 and 2\n");
 	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+		for(int j=0;j<n;j++){
 			printf("%d ",c[i][j]);
 		}
 	printf("\n");
