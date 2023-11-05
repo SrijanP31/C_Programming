679fda2f49f603481858903dbee2fc7e571f69de
SrijanP31 <143769604+SrijanP31@users.noreply.github.com>
2023-11-05 18:06:23 +0530

    Update mult_matrix.cpp

--- 16-Oct-23 Lab/mult_matrix.cpp
+++ 16-Oct-23 Lab/mult_matrix.cpp
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
 int main(){
-	int a[2][2], b[2][2];
-	int c[2][2] = {};
+	int n;
+	printf("Enter the value of n:");
+	scanf("%d",&n);
+	int a[n][n], b[n][n];
+	int c[n][n] = {};
 	printf("Enter values of matrix 1: \n");
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			scanf("%d",&a[i][j]);
 		}
 	}
 	printf("Enter values of matrix 2:\n");
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			scanf("%d",&b[i][j]);
 		}
 	}
 	
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
-			for(int k=0;k<2;k++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			for(int k=0;k<n;k++){
 			c[i][j] += (a[i][k] * b[k][j]);
 		}
 		}
 	}
 	printf("Product of matrices 1 and 2\n");
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			printf("%d ",c[i][j]);
 		}
 	printf("\n");
